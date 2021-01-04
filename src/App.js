e8fb4ed6219de8412df8fe962ba8f3dcdc1fdd6e
sanwalkhan <sanwalkhan388@yahoo.com>
2021-01-04 14:40:27 +0500

    change in App.js and menue.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Dinner from "./menue.js";
 function App() {
   return (
     <div>
-      <Dinner dishName = " Chicken Karhai" sweetDish ="kheer" />  
+      <Dinner dishName = " Chicken Karhai" sweetDish ="kheer" drink = "colddrink" />  
       <hr />
-      <Dinner dishName = " Mutton Karhai" sweetDish ="jalebi" />
+      <Dinner dishName = " Mutton Karhai" sweetDish ="jalebi" drink = "tea" />
       <hr /> 
-      <Dinner dishName = " Nehari Karhai" sweetDish ="halwa" /> 
+      <Dinner dishName = " Nehari Karhai" sweetDish ="halwa" drink = "Shake" /> 
     </div>
   )
 }
--- src/menue.js
+++ src/menue.js
@@ -5,6 +5,7 @@ function Dinner(props) {
         <div>
             <h1>Today We are Serving {props.dishName} </h1>
             <h1> and we serve {props.sweetDish} .</h1>
+            <h1>with {props.drink} </h1>
         </div>
     )
 }
